a91ae86c770b4719b0730251feb82e0d1bc9a1a7
EZEN-142 <EZEN-142@DESKTOP-88NAHRT>
2022-07-13 17:42:41 +0900

    2랑 같은 수만 누적합을 구하던 내용을 짝수만 누적합을 구하는 프로그램으로 변경

--- GitStudyTest1/src/ForApplyExam.java
+++ GitStudyTest1/src/ForApplyExam.java
@@ -3,22 +3,34 @@ public class ForApplyExam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int sum = 0;
+//		int sum = 0;
 		
-		for (int i = 0; i <= 10; i++) {
+//		for (int i = 0; i <= 10; i++) {
 //			System.out.println("i가 2인 경우에만 수행되는 조건문");
-			if(i == 2) {
-				sum = sum + i;
-				System.out.println("i의 현재 위치: " + i + " - 합계: " + sum);
-				
-				System.out.println();
-			}
+//			if(i == 2) {
+//				sum = sum + i;
+//				System.out.println("i의 현재 위치: " + i + " - 합계: " + sum);
+//				
+//				System.out.println();
+//			}
 			
 //			System.out.print("짝수의 합: " + sum);
 //			System.out.println();
-		}
 		
-		System.out.println("0~10까지의 짝수 합계는: " + sum);
+//		System.out.println("0~10까지의 짝수 합계는: " + sum);
+//		}
+		
+		int sum = 0; 
+		for(int i =1; i <=10; i++) {
+			if(i%2 == 0) {
+				sum = sum + i;
+			}
+			System.out.println("i의 현재 위치:" + i + " - 합계:" + sum);
+			
+			System.out.println("짝수의 합 : "+ sum);
+		}
+			
+		
 	}
 
 }
